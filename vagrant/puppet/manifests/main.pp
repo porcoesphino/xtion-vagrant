a7daed2b54edc72f3ed26f41cab7cc6690e8dc49
porcoesphino <bodeybaker@gmail.com>
2013-07-31 03:00:38 +0900

    Removed unneeded packages

--- vagrant/puppet/manifests/main.pp
+++ vagrant/puppet/manifests/main.pp
@@ -16,6 +16,11 @@ class othertools {
     ensure => latest,
     require => Exec["aptGetUpdate"]
   }
+  
+  package { "vim":
+    ensure => latest,
+    require => Exec["aptGetUpdate"]
+  }
 
   package { "curl":
     ensure => present,
@@ -38,21 +43,5 @@ class othertools {
   }
 }
 
-class heroku {
-  exec { "install-heroku":
-    command => "wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh",
-    creates => '/usr/bin/heroku',
-    path => ["/bin", "/usr/bin"],
-    require => Package['wget']
-  }
-}
-
-class { 'nodejs':
-  version => 'v0.8.9',
-  require => Package['g++', 'wget', 'git', 'curl']
-}
-
 include apt_update
 include othertools
-include heroku
-include nodejs
