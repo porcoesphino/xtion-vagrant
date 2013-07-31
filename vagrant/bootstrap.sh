c823f0c9fea64d820f1f0ab142b6eefc77cc11c7
porcoesphino <bodeybaker@gmail.com>
2013-07-31 21:17:02 +0900

    Populating bootstrap.sh

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -4,27 +4,29 @@
 sudo aptitude update -y
 
 # Build X since this we'll be spewing out video
-sudo apt-get install xauth
-sudo apt-get install xorg
-sudo apt-get install openbox
+sudo apt-get install -y xauth
+sudo apt-get install -y xorg
+sudo apt-get install -y openbox
 
 # It's quite possible one of these libraries assumes something from the desktop environment. List it here until that's been ruled out
 # sudo apt-get install ubuntu-desktop 
 
 # Build packages
 sudo aptitude install -y build-essential
+sudo aptitude install -y automake
 
 # Developement tools
+sudo aptitude install -y htop
 sudo aptitude install -y curl
+sudo aptitude install -y wget
 sudo aptitude install -y git
 sudo aptitude install -y vim
+sudo aptitude install -y emacs
+sudo aptitude install -y meld
+sudo aptitude install -y colordiff
+sudo aptitude install -y bash-completion
 
-sudo aptitude install -y htop
-sudo aptitude install -y automake
-emacs
-meld
-colordiff
-bash-completion
+# The Xtion libraries
 
-
-  
+# Configuration
+cd ~ && git clone https://github.com/porcoesphino/dotfiles && cd dotfiles && make install
