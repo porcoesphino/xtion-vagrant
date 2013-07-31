90f34fb61c8c77a8dbfb3f09933e0df1f3b4d070
Bodey Baker <bodey.baker@gmail.com>
2013-08-01 01:09:27 +0900

    Faster and more readble bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,30 +3,35 @@
 # Ensure up-to-date
 sudo aptitude update -y
 
+packagelist=(
 # Build X since this we'll be spewing out video
-sudo apt-get install -y xauth
-sudo apt-get install -y xorg
-sudo apt-get install -y openbox
+xauth
+xorg
+openbox
 
 # It's quite possible one of these libraries assumes something from the desktop environment. List it here until that's been ruled out
 # sudo apt-get install ubuntu-desktop 
 
 # Build packages
-sudo aptitude install -y build-essential
-sudo aptitude install -y automake
+build-essential
+automake
 
 # Developement tools
-sudo aptitude install -y htop
-sudo aptitude install -y curl
-sudo aptitude install -y wget
-sudo aptitude install -y git
-sudo aptitude install -y vim
-sudo aptitude install -y emacs
-sudo aptitude install -y meld
-sudo aptitude install -y colordiff
-sudo aptitude install -y bash-completion
+htop
+curl
+wget
+git
+vim
+emacs
+meld
+colordiff
+bash-completion
 
 # The Xtion libraries
+
+)
+
+sudo apt-get install -y ${packagelist[@]}
 
 # Configuration
 # A horrible hack, but we know the vagrant user
@@ -35,4 +40,6 @@ cd $HOME
 if [ ! -d $HOME/dotfiles ]; then
   sudo -u vagrant git clone https://github.com/porcoesphino/dotfiles
 fi
-cd $HOME/dotfiles && sudo -u vagrant git submodule update --init && sudo -u vagrant make install
+cd $HOME/dotfiles && \
+sudo -u vagrant git submodule update --init && \
+sudo -u vagrant make install
